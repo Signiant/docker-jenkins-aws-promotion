bb388e6a95ff03edeaa6d9b709cee7bf3597be6e
Mike Perttula <mperttula@signiant.com>
2017-09-19 08:43:39 -0400

    Modified Start.sh to use  docker-aws-promo script

--- start.sh
+++ start.sh
@@ -1,28 +1,86 @@
 #!/bin/bash
 
-# Just in case some of the tar files do not set correct owner
-#chown -R $BUILD_USER:$BUILD_USER_GROUP /home/$BUILD_USER
+GIT_CREDENTIALS_PARAM=PROMO.git-credentials
 
-if [ ! -z "$RUN_SLAVE" ]; then
-    echo "Downloading jenkins slave from $MASTER_ADDR"
-    wget -P / http://$MASTER_ADDR/jnlpJars/slave.jar
-    if [ -z "$SECRET" ]; then
-        su - $BUILD_USER -c "export HOME=/home/$BUILD_USER;\
-                             export JAVA_HOME=$JAVA_HOME;\
-                             export MAVEN_HOME=$MAVEN_HOME;\
-                             export OS=$SLAVE_OS;\
-                             export LC_ALL=en_US.UTF-8;\
-                             export LANG=en_US.UTF-8;\
-                             export LANGUAGE=en_US.UTF-8;\
-          java -jar /slave.jar -jnlpUrl http://$MASTER_ADDR/computer/$SLAVE_ID/slave-agent.jnlp"
-    else
-        su - $BUILD_USER -c "export HOME=/home/$BUILD_USER;\
-                             export JAVA_HOME=$JAVA_HOME;\
-                             export MAVEN_HOME=$MAVEN_HOME;\
-                             export OS=$SLAVE_OS;\
-                             export LC_ALL=en_US.UTF-8;\
-                             export LANG=en_US.UTF-8;\
-                             export LANGUAGE=en_US.UTF-8;\
-          java -jar /slave.jar -jnlpUrl http://$MASTER_ADDR/computer/$SLAVE_ID/slave-agent.jnlp -secret $SECRET"
-    fi
+AWS_PROMO_SCRIPTS_REPO_URL=https://github.com/Signiant/aws-promo-scripts.git
+
+CMD=$1
+ARTIFACT_PATH=/artifacts
+PROMOTION_REGION=us-west-2
+
+GIT_CREDENTIALS_PARAM=PROMO.git-credentials
+GIT_CRED=/credentials/.git-credentials
+NPM_CREDENTIALS_PARAM=PROMO.npmrc
+NPM_CRED=/credentials/.npmrc
+
+if [ ! -e "$ARTIFACT_PATH" ]; then
+    echo "Cannot find artifacts! Cannot continue"
+    exit 1
 fi
+
+get_credential()
+{
+    # $1 - parameter name for credential
+    PARAM_NAME=$1
+    # $2 - location to store credential
+    CRED_LOCATION=$2
+
+    mkdir -p /credentials
+
+    if [ -z "$CRED_LOCATION" ]; then
+        echo "Must provide a location to store the credential"
+        exit 1
+    else
+        # Go get credential from parameter store
+        credential=$(aws ssm get-parameters --names $PARAM_NAME --with-decryption --region $PROMOTION_REGION --query 'Parameters[0].Value' --output text)
+        if [ -e "$CRED_LOCATION" ]; then
+            echo "Warning: $CRED_LOCATION exists - overwriting..."
+        fi
+        echo $credential > $CRED_LOCATION
+        # Replace \n with newlines
+        sed -i 's/\\n/\n/g' $CRED_LOCATION
+    fi
+}
+
+# Get and set temporary AWS credentials
+get_temporary_aws_credentials()
+{
+    role_arn=$1
+    # echo "Geting temporary AWS credentials for role $role_arn"
+
+    assume_role_details=$(aws sts assume-role --role-arn ${role_arn} --role-session-name ASAP)
+
+    access_key_id=$(echo $assume_role_details | jq .Credentials.AccessKeyId -r)
+    secret_access_key=$(echo $assume_role_details | jq .Credentials.SecretAccessKey -r)
+    session_token=$(echo $assume_role_details | jq .Credentials.SessionToken -r)
+    # session_expiration=$(echo $assume_role_details | jq .Credentials.Expiration -r)
+
+    export AWS_ACCESS_KEY_ID=$access_key_id
+    export AWS_SECRET_ACCESS_KEY=$secret_access_key
+    export AWS_SESSION_TOKEN=$session_token
+}
+
+get_credential $GIT_CREDENTIALS_PARAM $GIT_CRED
+get_credential $NPM_CREDENTIALS_PARAM $NPM_CRED
+npm config set globalconfig /credentials/.npmrc
+
+mkdir aws-promo-scripts
+cd aws-promo-scripts
+git init . > /dev/null
+git config credential.helper 'store --file='$GIT_CRED
+git config remote.origin.url $AWS_PROMO_SCRIPTS_REPO_URL
+git pull > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to clone Git repo: $AWS_PROMO_SCRIPTS_REPO_URL"
+    exit 1
+fi
+
+# Run this with the appropriate role
+if [ ! -z "$ROLE_ARN" ]; then
+    get_temporary_aws_credentials $ROLE_ARN
+fi
+
+echo "Running the following command: $CMD"
+eval $CMD
+
+exit $?
